d8a01353c14b979ff3308dc6cc7f1cb76a820ddb
Oxore <oxore@protonmail.com>
2019-08-12 00:39:37 +0300

    Implement utf32to8_strncpy_s function

--- f8.c
+++ f8.c
@@ -35,6 +35,33 @@ static inline int32_t utf8to32_char(const char *c, size_t clen)
     }
 }
 
+static inline size_t utf32to8_char(char *buffer, int32_t code)
+{
+    if (code <= 0x7F) {
+        buffer[0] = code;
+        return 1;
+    }
+    if (code <= 0x7FF) {
+        buffer[0] = 0xC0 | (code >> 6);            /* 110xxxxx */
+        buffer[1] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+        return 2;
+    }
+    if (code <= 0xFFFF) {
+        buffer[0] = 0xE0 | (code >> 12);           /* 1110xxxx */
+        buffer[1] = 0x80 | ((code >> 6) & 0x3F);   /* 10xxxxxx */
+        buffer[2] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+        return 3;
+    }
+    if (code <= 0x10FFFF) {
+        buffer[0] = 0xF0 | (code >> 18);           /* 11110xxx */
+        buffer[1] = 0x80 | ((code >> 12) & 0x3F);  /* 10xxxxxx */
+        buffer[2] = 0x80 | ((code >> 6) & 0x3F);   /* 10xxxxxx */
+        buffer[3] = 0x80 | (code & 0x3F);          /* 10xxxxxx */
+        return 4;
+    }
+    return 0;
+}
+
 size_t utf8_strlen(const char *string)
 {
     if (string == NULL)
@@ -50,11 +77,25 @@ size_t utf8_strlen(const char *string)
 char *utf32to8_strncpy_s(char *dest, size_t destsz, const int32_t *src,
         size_t n)
 {
-    (void) dest;
-    (void) destsz;
-    (void) src;
-    (void) n;
-    return NULL;
+    if (dest == NULL || src == NULL)
+        return dest;
+
+    char c[4];
+    size_t i = 0;
+    for (; n && *src && i + 1 < destsz; n--, src++) {
+        size_t clen = utf32to8_char(c, *src);
+        if (clen == 0)
+            break; // Probably an invalid character
+        if (i + clen >= destsz)
+            break; // Does not fit alongside with NULL terminator
+        memcpy(&dest[i], c, clen);
+        i += clen;
+    }
+
+    if (destsz >= sizeof(char))
+        dest[i] = 0;
+
+    return dest;
 }
 
 int32_t *utf8to32_strncpy_s(int32_t *dest, size_t destsz, const char *src,
